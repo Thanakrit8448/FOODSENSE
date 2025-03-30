db8928cf07e847181849ad8bcb36c3a8882362e4
Thanakrit8448 <icekung8448@gmail.com>
2025-03-30 14:05:37 +0700

    create Login page
    -แก้ UI หน้าเก่า
    -เพิ่ม fonts ใหม่
    
    TODO:
    -สร้างหน้า SIGNUP page
    -ระบบ register

--- foodsense_app/lib/main.dart
+++ foodsense_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,80 +10,88 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false, //ปิดแทบ debug ขวาบน
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FOODSENSE',
       theme: ThemeData(
         primaryColor: Color(0xFFAD6FFF),
       ),
-      home: Scaffold(
-        backgroundColor: Color(0xFFAD6FFF),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Spacer(),
-              Text('FOODSENSE',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: EdgeInsets.only(left: 40),
-                child: Image.asset(
-                  'assets/icons/Logo.png',
-                  height: 200,
-                ),
-              ),
-              // const SizedBox(height: 10),
-              // Text('Smart food map Smart choices!',
-              //   style: TextStyle(
-              //     fontSize: 16,
-              //     fontStyle: FontStyle.italic,
-              //     color: Colors.white, 
-              //   ),
-              // ),
-              const Spacer(),
+      home: const HomePage(), // เปลี่ยนจาก Scaffold เป็น HomePage()
+    );
+  }
+}
 
-              //const SizedBox(height: 10),
-              Text('Smart food map Smart choices!',
-                style: TextStyle(
-                  fontFamily: 'Caveat',
-                  fontSize: 24,
-                  // fontStyle: FontStyle.italic,
-                  color: Colors.white, 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFAD6FFF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2,),
+            Text(
+              'FOODSENSE',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 0),
+            Padding(
+              padding: EdgeInsets.only(left: 70),
+              child: Image.asset(
+                'assets/icons/Logo.png',
+                height: 270,
+              ),
+            ),
+            const Spacer(),
+            Text(
+              'Smart food map Smart choices!',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 24,
+                color: Colors.white,
+              ),
+            ),
+            //const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              child: SizedBox(
+                width: double.infinity,
+                height: 70,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginPage()),
+                    );
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 65,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                      )
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Get Strated',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      )
-                    ),
-                  )
-                ),)
-            ],
-          ),
+            ),
+            const SizedBox(height: 50),
+          ],
         ),
       ),
     );
---
+++ foodsense_app/lib/pages/login_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key}); // ✅ เพิ่ม Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFAD6FFF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 70), 
+              child: Text(
+              'FOODSENSE',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Spacer(),
+            
+            Text(
+              'Welcome Back',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 45,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          side: BorderSide(
+                            color: Color(0xFFD091ED), //D091ED
+                            width: 2.0),
+                          
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'SIGN IN',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFAD6FFF),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 350),
+          ],
+        ),
+      ),
+    );
+  }
+}
